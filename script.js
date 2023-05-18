e414b33865e209558cc0ee7e9ad0d1b069565662
Lorenzo Taglio <lorenzo.taglio.work@gmail.com>
2023-05-18 09:59:59 +0200

    Add tile class to grid divs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const gridContainer = document.querySelector(".grid-container");
 function createGrid() {
     for(let i=0;i<16*16;i++){
         let tile = document.createElement("div");
+        tile.classList.add("tile");
         gridContainer.appendChild(tile);
     }
 }
